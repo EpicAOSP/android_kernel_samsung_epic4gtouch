0b4bcd83b18565091be09fe079a33f84440fa9f6
Andrew Dodd <atd7@cornell.edu>
2012-04-04 09:24:58 -0400

    exynos4: Disable SW downclock in cpuidle
    The Samsung 3.0.x source includes a new exynos4 feature:
    reducing CPU clock when in idle.  However, this seems
    pointless as the CPU clock gets gated in this state anyway.
    
    Also, the logic for doing this seems to sometimes hang,
    as there are occasional cases of the RCU stall detector
    going off due to CPU1 getting stuck in exynos4_enter_idle()
    
    This issue seems to go away when disabling the SW downclock
    feature with not much negative effect on battery, so disable
    it for now until it can be revisited.
    
    Signed-off-by: Andrew Dodd <atd7@cornell.edu>

--- arch/arm/mach-exynos/cpuidle-exynos4.c
+++ arch/arm/mach-exynos/cpuidle-exynos4.c
@@ -739,6 +739,7 @@ static int exynos4_enter_idle(struct cpuidle_device *dev,
 	local_irq_disable();
 	do_gettimeofday(&before);
 
+#if 0
 	if (use_clock_down == SW_CLK_DWN) {
 		/* USE SW Clock Down */
 		cpu = get_cpu();
@@ -778,6 +779,7 @@ static int exynos4_enter_idle(struct cpuidle_device *dev,
 
 		put_cpu();
 	} else
+#endif
 		cpu_do_idle();
 
 	do_gettimeofday(&after);
